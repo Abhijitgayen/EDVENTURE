af68543cf66ead9c8c1ccbd4efbc39096c10681c
agayen <abhijitagyen428@gmail.com>
2021-12-13 15:03:05 +0530

    update some css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ export default class Footer extends Component {
                 <div className='footer'>
                     <Container>
                         <Row md className="footer_all justify-content-center">
-                            <Col className=" col-6 col-sm-6 col-md-4   footer_card">
+                            <Col className=" col-sm-12 col-md-4 col-12  footer_card">
                                 <h2>COURSES</h2>
                                 <ul>
                                     <li>GATE</li>
@@ -18,7 +18,7 @@ export default class Footer extends Component {
                                     <li>Free study Matrial</li>
                                 </ul>
                             </Col>
-                            <Col className="col-6 col-sm-6 col-md-4  footer_card ">
+                            <Col className="col-sm-12 col-md-4 col-12 footer_card ">
                                 <h2>COMPANY</h2>
                                 <ul>
                                     <li>About Us</li>
@@ -28,7 +28,7 @@ export default class Footer extends Component {
                                     <li>Terms and Services</li>
                                 </ul>
                             </Col>
-                            <Col className="col-6 col-sm-6 col-md-4   footer_card" >
+                            <Col className="col-sm-12 col-md-4 col-12  footer_card" >
                                 <h2>OTHERS</h2>
                                 <ul>
                                     <li>Ambassadors</li>
@@ -39,7 +39,7 @@ export default class Footer extends Component {
                                     <li>Scholarships</li>
                                 </ul>
                             </Col>
-                            <Col className="col-6 col-sm-6 col-md-4  footer_card">
+                            <Col className="col-sm-12 col-md-4 col-12 footer_card">
                                 <h2 className='follow_us_t'>FOLLOW US</h2>
                                 <div className="follow_us" >
                                     <FaFacebookSquare size="35px" className='footer_icon' />
--- src/components/ModelSet.js
+++ src/components/ModelSet.js
@@ -1,33 +1,53 @@
-import React ,{useState} from 'react';
-import {Modal,Button} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Modal, Button, Form, FormLabel, Container, Row, Col } from 'react-bootstrap';
 
-export default function ModelSet(props){
-    const [show, setShow] = useState(false);
-  
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-      <>
-        <Button variant="primary" onClick={handleShow}>
-          {props.btn_name}{props.icon}
-        </Button>
-  
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
+export default function ModelSet(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        {props.btn_name}{props.icon}
+      </Button>
+
+      <Modal show={show} onHide={handleClose} className='model_bg'>
+        <Modal.Header closeButton>
+          <Modal.Title>SIGN IN</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Form>
+              <Row className="col-12">
+                <Col className="col-4">
+                  <FormLabel>User ID :</FormLabel>
+                </Col>
+                <Col className="col-8">
+                  <input></input>
+                </Col>
+                <Col className="col-4">
+                <FormLabel>Password :</FormLabel>
+                </Col>
+                <Col className="col-8">
+                <input type='password'></input>
+                </Col>
+              </Row>
+            </Form>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          
+          <Button variant="primary" onClick={handleClose}>
+            Sign In
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Log In
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 
