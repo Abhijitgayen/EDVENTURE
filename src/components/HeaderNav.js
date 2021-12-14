e6ed417809764e3efc822486059555a98412b838
agayen <abhijitagyen428@gmail.com>
2021-12-14 22:44:33 +0530

    css work 4

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -17,26 +17,26 @@ export class HeaderNav extends Component {
                         <Navbar.Brand href="#home">EDVENTURE LOGO</Navbar.Brand>
 
                         <Navbar.Collapse id="responsive-navbar-nav" >
-                            <Nav className="ms-auto">
+                            <Nav className="ms-auto" id='header_nav_link'>
                                 <Nav.Link href="#Home" className='nav_link active'> Home </Nav.Link>
-                                <Nav.Link className='nav_link'>
                                 <Dropdown>
                                     <Dropdown.Toggle  size="sm" variant='light' id="drop_toggle">
                                         Courses
                                     </Dropdown.Toggle>
 
                                     <Dropdown.Menu id="drop_bg" variant="dark">
-                                    <Dropdown.Item id="dropdown_item" href="#action/3.1"  >Gate</Dropdown.Item>
-                                    <Dropdown.Item id="dropdown_item" href="#action/3.2"  >Engineering</Dropdown.Item>
-                                    <Dropdown.Item id="dropdown_item"  href="#action/3.3" >10th and 12th</Dropdown.Item>
+                                    <Dropdown.Item id="dropdown_item" variant="secondary" href="#action/3.1"  >Gate Exam</Dropdown.Item>
+                                    <Dropdown.Item id="dropdown_item" href="#action/3.2"  >JAM Exam</Dropdown.Item>
+                                    <Dropdown.Item id="dropdown_item"  href="#action/3.3" >CS Exam</Dropdown.Item>
+                                    <Dropdown.Item id="dropdown_item"  href="#action/3.4" >Online Tutor</Dropdown.Item>
+
                                     <Dropdown.Divider />
-                                    <Dropdown.Item id="dropdown_item" href="#action/3.4" >Free Study Material</Dropdown.Item>   
+                                    <Dropdown.Item id="dropdown_item" href="#action/3.5" >Free Study Material</Dropdown.Item>   
                                     </Dropdown.Menu>
                                 </Dropdown>
-                                </Nav.Link>
-                                <Nav.Link href="#free_study" className='nav_link'>About Us</Nav.Link>
+                                <Nav.Link href="#aboutUs" className='nav_link'>About Us</Nav.Link>
                                 <Nav.Link href="#team" className='nav_link'>Team</Nav.Link>
-                                <Nav.Link href="#results" className='nav_link'>Results</Nav.Link>
+                                <Nav.Link href="#blogs" className='nav_link'>Blogs</Nav.Link>
                                 <Nav.Link href="#contact" className='nav_link'>Contact</Nav.Link>
                                 <Nav.Link eventKey={2} href="#sign_up" >
                                     {/* <Button> SIGN UP <FaSignInAlt className="sign_icon"/></Button>  */}
