96063d6f4bd6de79f7cdc2a013e1fb774a8c7d80
agayen <abhijitagyen428@gmail.com>
2021-12-15 18:28:14 +0530

    home work 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
                     <header className="home_top" id="set-for-bg">
                         <Container>
                             <Row>
-                                <Col className="col-12 col-sm-6 col-md-6">
+                                <Col className="home_top_text col-12 col-sm-6 col-md-6">
                                     <h1>EMBARK ON AN ADVENTURE TO MORDERN INTERACTIVE EDUCATION</h1>
                                     <p>Handcrafted at IIT , Edventure aim to impart rich Courses helping Students in academics as well as competitative Exams</p>
                                     <Button variant="light" >VENTURE NOW</Button>
@@ -17,13 +17,26 @@ export default function Home() {
                                 <Col className="col-12 col-sm-6 col-md-6">
                                     <img src={online_learning} alt='online_learning' id='home_img' />
                                 </Col>
-
                             </Row>
                         </Container>
                     </header>
-                    <Container>
-                        <Row>
-                            <Col>
+                    <Container className='home_mid_f'>
+                        <Row className='justify-content-center'>
+                            <Col className='home_card col-5 col-md-2'>
+                            <h1>250+</h1>
+                            <p>Students </p>
+                            </Col>
+                            <Col className='home_card col-5 col-md-2'>
+                            <h1>50+</h1>
+                            <p>Top IITs Tutor</p>
+                            </Col>
+                            <Col className='home_card col-5 col-md-2'>
+                            <h1>25+</h1>
+                            <p>Course Avaliable</p>
+                            </Col>
+                            <Col className='home_card col-5 col-md-2'>
+                            <h1>80%</h1>
+                            <p>Better Enhanced Programs</p>
                             </Col>
                         </Row>
                     </Container>
