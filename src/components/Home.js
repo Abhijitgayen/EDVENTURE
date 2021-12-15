39ba23242b832b3043376d2cf5959bdee0c0db09
agayen <abhijitagyen428@gmail.com>
2021-12-15 19:39:03 +0530

    css error se

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,12 @@ export default function Home() {
                 <header className="home_top" id="set-for-bg">
                     <Container>
                         <Row>
-                            <Col className="home_top_text col-12 col-sm-6 col-md-6">
+                            <Col className="home_top_text col-12 col-sm-12 col-md-8 col-xl-6">
                                 <h1>EMBARK ON AN ADVENTURE TO MORDERN INTERACTIVE EDUCATION</h1>
                                 <p>Handcrafted at IIT , Edventure aim to impart rich Courses helping Students in academics as well as competitative Exams</p>
                                 <Button variant="light" >VENTURE NOW</Button>
                             </Col >
-                            <Col className="col-12 col-sm-6 col-md-6">
+                            <Col className="col-12 col-sm-12 col-md-6 offset-md-6 col-xl-6 offset-xl-0">
                                 <img src={online_learning} alt='online_learning' id='home_img' />
                             </Col>
                         </Row>
@@ -22,19 +22,19 @@ export default function Home() {
                 </header>
                 <Container className='home_mid_f'>
                     <Row className='justify-content-center'>
-                        <Col className='home_card col-5 col-md-2'>
+                        <Col className='home_card col-10 col-sm-5 col-md-2'>
                             <h1>250+</h1>
                             <p>Students </p>
                         </Col>
-                        <Col className='home_card col-5 col-md-2'>
+                        <Col className='home_card col-10 col-sm-5 col-md-2'>
                             <h1>50+</h1>
                             <p>Top IITs Tutor</p>
                         </Col>
-                        <Col className='home_card col-5 col-md-2'>
+                        <Col className='home_card col-10 col-sm-5 col-md-2'>
                             <h1>25+</h1>
                             <p>Course Avaliable</p>
                         </Col>
-                        <Col className='home_card col-5 col-md-2'>
+                        <Col className='home_card col-10 col-sm-5 col-md-2'>
                             <h1>80%</h1>
                             <p>Better Enhanced Programs</p>
                         </Col>
