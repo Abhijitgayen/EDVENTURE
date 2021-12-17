08bc30cb5b9b500f40fb1564ff7a6b5c202caf84
agayen <abhijitagyen428@gmail.com>
2021-12-17 16:22:17 +0530

    update 1

--- src/components/AllCourse.js
+++ src/components/AllCourse.js
@@ -10,108 +10,108 @@ export class AllCourse extends Component {
                     <Accordion defaultActiveKey="0">
                         <Accordion.Item eventKey="0">
                             <Row>
-                                <Col className='col-12 col-sm-12 col-md-4 order-md-last'>
-                                    <Accordion.Header >Accordion Item #1</Accordion.Header>
+                                <Col className='col-12 col-sm-12 col-md-4'>
+                                    <Accordion.Header >Undergraduate Foundations</Accordion.Header>
                                 </Col>
-                                <Col className='co_set_1 col-12 col-sm-12 col-md-8 order-md-first'>
+                                <Col className='co_set_1 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body' >
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                        est laborum.
+                                        <h4>Undergraduate Engineering and Science Streams</h4>
+                                        <ul>
+                                            <li>
+                                                Learn core technical concepts of Engineering and Science, semester wise along with your college for building strong foundations and excel in semester exams with max CGPA.
+                                            </li>
+                                            <li>
+                                                Carefully designed course catering to different state and national boards and universities.
+                                            </li>
+                                        </ul>
                                     </Accordion.Body>
                                 </Col>
                             </Row>
                         </Accordion.Item>
                         <Accordion.Item eventKey="1">
                             <Row>
-                                <Col className='col-12 col-sm-12 col-md-4 order-md-last' >
-                                    <Accordion.Header >Accordion Item #2</Accordion.Header>
+                                <Col className='col-12 col-sm-12 col-md-4' >
+                                    <Accordion.Header >Postgraduate Foundation Program</Accordion.Header>
                                 </Col>
-                                <Col className='co_set_2 col-12 col-sm-12 col-md-8 order-md-first'>
+                                <Col className='co_set_2 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body'>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                        est laborum.
+                                        <h4>Postgraduate Foundation Program</h4>
+                                        <ul>
+                                            <li>
+                                                Learn core concepts of Engineering and Science to get deeper insights in research and development and the best placement opportunities.
+                                            </li>
+                                            <li>
+                                                Carefully designed course catering to different state and national boards and universities.
+                                            </li>
+                                        </ul>
                                     </Accordion.Body>
                                 </Col>
                             </Row>
                         </Accordion.Item>
                         <Accordion.Item eventKey="2">
                             <Row>
-                                <Col className='col-12 col-sm-12 col-md-4 order-md-last' >
-                                    <Accordion.Header >Accordion Item #3</Accordion.Header>
+                                <Col className='col-12 col-sm-12 col-md-4' >
+                                    <Accordion.Header >College Final Year Projects</Accordion.Header>
                                 </Col>
-                                <Col className='co_set_3 col-12 col-sm-12 col-md-8 order-md-first'>
+                                <Col className='co_set_3 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body'>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                        est laborum.
+                                        <h4>College Final Year Projects</h4>
+                                        <ul>
+                                            <li>
+                                                Application based projects which help students apply theoretical knowledge to build industrial applications and research oriented tasks.
+                                            </li>
+                                            <li>
+                                                Placement intensive projects to boost your resume.
+                                            </li>
+                                        </ul>
                                     </Accordion.Body>
                                 </Col>
                             </Row>
                         </Accordion.Item>
                         <Accordion.Item eventKey="3">
                             <Row>
-                                <Col className='col-12 col-sm-12 col-md-4 order-md-last' >
-                                    <Accordion.Header >Accordion Item #4</Accordion.Header>
+                                <Col className='col-12 col-sm-12 col-md-4' >
+                                    <Accordion.Header >Interview and Placement Preparation</Accordion.Header>
                                 </Col>
-                                <Col className='co_set_4 col-12 col-sm-12 col-md-8 order-md-first'>
+                                <Col className='co_set_4 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body'>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                        est laborum.
+                                        {' '}
                                     </Accordion.Body>
                                 </Col>
                             </Row>
                         </Accordion.Item>
                         <Accordion.Item eventKey="4">
                             <Row>
-                                <Col className='col-12 col-sm-12 col-md-4 order-md-last' >
-                                    <Accordion.Header >Accordion Item #5</Accordion.Header>
+                                <Col className='col-12 col-sm-12 col-md-4' >
+                                    <Accordion.Header >GATE/ES</Accordion.Header>
                                 </Col>
-                                <Col className='co_set_5 col-12 col-sm-12 col-md-8 order-md-first'>
+                                <Col className='co_set_5 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body'>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                        est laborum.
+                                        {' '}
                                     </Accordion.Body>
                                 </Col>
                             </Row>
                         </Accordion.Item>
                         <Accordion.Item eventKey="5">
                             <Row>
-                                <Col className='col-12 col-sm-12 col-md-4 order-md-last' >
-                                    <Accordion.Header >Accordion Item #6</Accordion.Header>
+                                <Col className='col-12 col-sm-12 col-md-4' >
+                                    <Accordion.Header >Computer Programming</Accordion.Header>
                                 </Col>
-                                <Col className='co_set_6 col-12 col-sm-12 col-md-8 order-md-first'>
+                                <Col className='co_set_6 col-12 col-sm-12 col-md-8 '>
                                     <Accordion.Body className='allCo_body' >
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                                        est laborum.
+                                        {' '}
+                                    </Accordion.Body>
+                                </Col>
+                            </Row>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="6">
+                            <Row>
+                                <Col className='col-12 col-sm-12 col-md-4' >
+                                    <Accordion.Header >Schools</Accordion.Header>
+                                </Col>
+                                <Col className='co_set_6 col-12 col-sm-12 col-md-8'>
+                                    <Accordion.Body className='allCo_body' >
+                                        {' '}
                                     </Accordion.Body>
                                 </Col>
                             </Row>
