4c0e0b9e92495895dc54565e7a44ae1a05984f7a
agayen <abhijitagyen428@gmail.com>
2021-12-18 19:44:28 +0530

    demo done

--- src/components/AllCourse.js
+++ src/components/AllCourse.js
@@ -15,8 +15,8 @@ export class AllCourse extends Component {
                                 </Col>
                                 <Col className='co_set_1 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body' >
-                                        <h4>Undergraduate Engineering and Science Streams</h4>
                                         <ul>
+                                            <h4>Undergraduate Engineering and Science Streams</h4>
                                             <li>
                                                 Learn core technical concepts of Engineering and Science, semester wise along with your college for building strong foundations and excel in semester exams with max CGPA.
                                             </li>
@@ -35,8 +35,8 @@ export class AllCourse extends Component {
                                 </Col>
                                 <Col className='co_set_2 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body'>
-                                        <h4>Postgraduate Foundation Program</h4>
                                         <ul>
+                                            <h4>Postgraduate Foundation Program</h4>
                                             <li>
                                                 Learn core concepts of Engineering and Science to get deeper insights in research and development and the best placement opportunities.
                                             </li>
@@ -55,8 +55,8 @@ export class AllCourse extends Component {
                                 </Col>
                                 <Col className='co_set_3 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body'>
-                                        <h4>College Final Year Projects</h4>
                                         <ul>
+                                            <h4>College Final Year Projects</h4>
                                             <li>
                                                 Application based projects which help students apply theoretical knowledge to build industrial applications and research oriented tasks.
                                             </li>
@@ -75,7 +75,12 @@ export class AllCourse extends Component {
                                 </Col>
                                 <Col className='co_set_4 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body'>
-                                        {' '}
+                                        <ul>
+                                            <h4>Interview and Placement Preparation</h4>
+                                            <li>
+                                                The best in class company specific interview and placement preparation to help aspiring students and professionals crack the selection criteria of top product and service-based companies.
+                                            </li>
+                                        </ul>
                                     </Accordion.Body>
                                 </Col>
                             </Row>
@@ -83,11 +88,19 @@ export class AllCourse extends Component {
                         <Accordion.Item eventKey="4">
                             <Row>
                                 <Col className='col-12 col-sm-12 col-md-4' >
-                                    <Accordion.Header >GATE/ES</Accordion.Header>
+                                    <Accordion.Header >GATE/ES/JAM</Accordion.Header>
                                 </Col>
                                 <Col className='co_set_5 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body'>
-                                        {' '}
+                                        <ul>
+                                            <h4>GATE / ES / JAM</h4>
+                                            <li>
+                                                One stop solution to ace GATE / ESE and other PSU examinations by mastering the technical foundations through an intensive course taught by the top graduates from IIT, NIT and other prestigious colleges.
+                                            </li>
+                                            <li>
+                                                Our tutors go through continuous assessment and regular updating about the current patterns of competitive exams.
+                                            </li>
+                                        </ul>
                                     </Accordion.Body>
                                 </Col>
                             </Row>
@@ -99,7 +112,18 @@ export class AllCourse extends Component {
                                 </Col>
                                 <Col className='co_set_6 col-12 col-sm-12 col-md-8 '>
                                     <Accordion.Body className='allCo_body' >
-                                        {' '}
+                                        <ul>
+                                            <h4>Computer Programming</h4>
+                                            <li>
+                                                Learn to write the most efficient computer programs with the best practices from scratch to tackle the competitions like CodeChef, Hackerearth, Hackerrank etc and gain an edge in the interview process.
+                                            </li>
+                                            <li>
+                                                Taught by the best coding professionals from top tier colleges with real world projects.
+                                            </li>
+                                            <li>
+                                                All industry and market friendly languages like Python, Java, C++ etc. are covered.
+                                            </li>
+                                        </ul>
                                     </Accordion.Body>
                                 </Col>
                             </Row>
@@ -111,7 +135,21 @@ export class AllCourse extends Component {
                                 </Col>
                                 <Col className='co_set_6 col-12 col-sm-12 col-md-8'>
                                     <Accordion.Body className='allCo_body' >
-                                        {' '}
+                                        <ul>
+                                            <h4>Schools</h4>
+                                            <li>
+                                                Tailor made comprehensive solutions catering to the students and professionals to help change their careers into lucrative and demanding technical domains like Machine Learning, Data Science, Full Stack Development etc.
+                                            </li>
+                                            <li>
+                                                The technical courses are divided into modules containing appropriate theory and intensive practical along with real time case studies.
+                                            </li>
+                                            <li>
+                                                Switch careers to high impact business domains and increase your salary or chance of promotion exponentially.
+                                            </li>
+                                            <li>
+                                                Connect with industry leaders for personal feedback and 1 to 1 mentorship to land the best jobs along with career growth.
+                                            </li>
+                                        </ul>
                                     </Accordion.Body>
                                 </Col>
                             </Row>
--- src/components/DemoClass.js
+++ src/components/DemoClass.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Col, Container, Form, Row } from 'react-bootstrap'
+import { Button, Accordion, Col, Container, Form, Row } from 'react-bootstrap'
 import online_video from '../images/online_video.svg'
 
 function DemoClass() {
@@ -7,35 +7,85 @@ function DemoClass() {
         <>
             <Container>
                 <Row>
-                    <Col className='demo_text col-12 col-sm-12 col-md-6'>
+                    <Col className='demo_text col-12 col-sm-12 col-md-5 col-xl-6'>
+                        <Row className='demp_det'>
+                            <Col className='col-4 '>
+                                <img src={online_video} alt='' className='demo_img'/>
+                                <p>Live 1:1 session from The best tutors</p>
+                            </Col>
+                            <Col className='col-4'>
+                                <img src={online_video} alt='' className='demo_img'/>
+                                <p>Interactive Learning With visualizations and Assessments</p>
+                            </Col>
+                            <Col className='col-4'>
+                                <img src={online_video} alt='' className='demo_img'/>
+                                <p>Most affordable Price</p>
+                            </Col>
+                        </Row>
+                        <Accordion defaultActiveKey="0" flush style={{ background: 'yellow' }} >
+                            <Accordion.Item eventKey="0">
+                                <Accordion.Header><h6>Personalized learning and Mentorship </h6></Accordion.Header>
+                                <Accordion.Body>
+                                    Edventure offers intensive online courses catering to students’ requirements and calibre. We aim to provide tailor made courses as well as mentorship to nurture young talents.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="1">
+                                <Accordion.Header><h6>Unparalleled Doubt Clearing and Evaluation </h6></Accordion.Header>
+                                <Accordion.Body>
+                                    We offer separate doubt clearing sessions for every module through 1:1 interaction and learning communities. Continuous assessments with performance evaluation and feedbacks brings the best out of students.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="2">
+                                <Accordion.Header><h6>The Best Lecture Notes and Study Materials</h6></Accordion.Header>
+                                <Accordion.Body>
+                                    Our classroom program is supported with the best study materials and notes which enhances the practicality and understanding of the subject.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="3">
+                                <Accordion.Header><h6>Motivation Sessions with IITians</h6></Accordion.Header>
+                                <Accordion.Body>
+                                    We host periodic motivation sessions with fellow IITians about college life and job prospects for our students to gear up for GATE like competitive exams and get selected in top tier colleges.
+                                </Accordion.Body>
+                            </Accordion.Item>
+                            <Accordion.Item eventKey="4">
+                                <Accordion.Header><h6>Most Affordable Courses</h6></Accordion.Header>
+                                <Accordion.Body>
+                                    Our courses are priced aggressively with attractive referral programs to ensure one’s career path does not get hindered by finance.
+                                </Accordion.Body>
+                            </Accordion.Item>
+
+                        </Accordion>
 
                     </Col>
-                    <Col className=' demo_from col-12 col-sm-12 col-md-6 '>
-                    <Row className='align-item-center'>
-                        <Col className='col-7 col-sm-7 '>
-                    <p>Book a FREE Sesion For You</p>
-                    <ul>
-                        <li>1-Hour LIVE SESSION with our Expert Tutor</li>
-                        <li>Visual Curriculum aligned To CBSE and ICSE</li>
-                    </ul>
-                        </Col>
-                        <Col className='col-5 col-sm-5' >
-                    <img src={online_video} alt='none' className='online_video'/>
-                        </Col>
-                    </Row>
-                    <Form>
-                        <div className='demo_input'>
-                        <input type='text' className='col-12 offset-sm-1 col-sm-10' placeholder='Enter Your Name'/>
-                        </div>
-                        <div className='demo_input'>
-                        <input type="email" className='col-12 offset-sm-1 col-sm-10' placeholder='Enter your email id'/>
-                        </div>
-                        <div className='demo_input'>
-                        <input type='number' className='col-12 offset-sm-1 col-sm-10' placeholder='Enter your Mobile number'/>
-                        </div>
-                        <div className='demo_input'><input type='checkbox'></input> Get important updates on WhatsApp</div>
-                        <Button variant='outline-warning ' id='demo_btn' size='md' className='col-6 offset-6 offset-md-1 ' >Submit</Button> 
-                    </Form>
+                    <Col className=' demo_from col-11 col-sm-11 col-md-7 col-xl-6'>
+                        <h4>Book your free demo class</h4>
+                        <Row className='align-item-center'>
+                            <Col className='col-4 col-sm-4' >
+                                <img src={online_video} alt='none' className='online_video' />
+                            </Col>
+                            <Col className='col-8 col-sm-7 '>
+                                <ul>
+                                    <li>1-Hour LIVE SESSION with our Expert Tutor</li>
+                                    <li>Visual Curriculum aligned To CBSE and ICSE</li>
+                                </ul>
+                            </Col>
+                        </Row>
+                        <Form>
+                            <div className='demo_input'>
+                                <label className='offset-sm-1'>Name</label>
+                                <input type='text' className='col-12 offset-sm-1 col-sm-10' placeholder='Enter Your Name' />
+                            </div>
+                            <div className='demo_input'>
+                                <label className='offset-sm-1'>Email ID</label>
+                                <input type="email" className='col-12 offset-sm-1 col-sm-10' placeholder='Enter your email id' />
+                            </div>
+                            <div className='demo_input'>
+                                <label className='offset-sm-1'>Mob. No</label>
+                                <input type='number' className='col-12 offset-sm-1 col-sm-10' placeholder='Enter your Mobile number' />
+                            </div>
+                            <div className='demo_input offset-sm-1'><input type='checkbox'></input> Get important updates on WhatsApp</div>
+                            <Button variant='outline-warning ' id='demo_btn' size='md' className='col-5 offset-7 offset-sm-6 offset-md-1 ' >Submit</Button>
+                        </Form>
                     </Col>
                 </Row>
             </Container>
